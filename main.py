e7bf49f8a65f75ec5c360215c2c8998127b11a84
acortinovis <adelio.cortinovis@gmail.com>
2025-03-05 10:34:06 -0800

    pseudocodes
    created pseudocodes before starting to code

---
+++ main.py
@@ -0,0 +1,23 @@
+#Atbash Cipher
+#ask the user whether they want to decrypt or encrypt a message
+#Accept a string that the program will either encrypt or decrypt.
+
+
+#Create two functions that will accept parameters in the form of a list or a dictionary that will contain the string meant to be encrypted or decrypted.
+#one function will encrypt
+
+
+#the other will decrypt using the Atbash cipher technique.
+
+
+# This should involve both loops and if statements.
+
+
+
+#Either call a function to encrypt or to decrypt the string.
+
+
+
+
+#Present the final encrypted or decrypted string to the user.
+#The user must be able to copy that output and run it through the program again to perfectly encrypt or decrypt it multiple times without any mistakes
